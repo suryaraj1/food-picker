3cf6d3bb093b077f98750477477f283876576fd0
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-09-07 16:12:32 +0530

    Fix content shift

--- src/components/RightSection/RightSection.js
+++ src/components/RightSection/RightSection.js
@@ -1,31 +1,84 @@
-import React from 'react';
-import FoodCard from '../FoodCard/FoodCard';
-import './RightSection.css';
+import React from "react";
+import EmojiMeaning from "../EmojiMeaning/EmojiMeaning";
+import FoodCard from "../FoodCard/FoodCard";
+import "./RightSection.css";
 
-const foodEmoticons = [
-    "🍪", "🍎", "🍖", "🍨", "🍯", "🍜", "🌭", "🍕", "🌮", "🍟", "🍱", "🥗"
-]
+const foodEmoticons = {
+    "🍖": "Meat on Bone",
+    "🍔": "Hamburger",
+    "🥟": "Dumpling",
+    "🍝": "Spaghetti",
+    "🥦": "Brocolli",
+    "🍟": "French Fries",
+    "🍦": "Soft Ice Cream",
+    "🍭": "Lollipop",
+    "🍕": "Pizza",
+    "🥝": "Kiwi Fruit",
+    "🥓": "Bacon",
+    "🌭": "Hot Dog",
+};
 
 class RightSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            foodEmoji: "",
+            emojiMeaning: "",
+            clickCount: 0
+        };
+    }
+
+    emojiHandler = (emoji) => {
+        this.setState(
+            {
+                foodEmoji: emoji,
+                emojiMeaning: foodEmoticons[emoji],
+                clickCount: this.state.clickCount + 1
+            }
+        );
+    };
+
     render() {
+        const { foodEmoji, emojiMeaning, clickCount } = this.state;
         return (
-            <div className='right-section-wrapper'>
-                <div className='input-wrapper'>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            <div className="right-section-wrapper">
+                <div className="input-wrapper">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                        />
                     </svg>
-                    <input placeholder="Enter a food emoji"/>
+                    <input placeholder="Enter a food emoji" />
                 </div>
-                <div className='food-card-grid'>
-                    <div className='selection-text-wrapper'>
-                        <p className='selection-text'>Pick a Food Emoji</p>
+                <div className='emoji-meaning-container'>
+                    {clickCount > 0 && <EmojiMeaning emoji={foodEmoji} emojiMeaning={emojiMeaning}/>}
+                </div>
+                <div className="food-card-grid">
+                    <div className="selection-text-wrapper">
+                        <p className="selection-text">Pick a Food Emoji</p>
                     </div>
-                    <div className='food-card-container'>
-                        {foodEmoticons.map(emoji => <FoodCard foodIcon={emoji}/>)}
+                    <div className="food-card-container">
+                        {Object.keys(foodEmoticons).map((emoji) => {
+                            return (
+                                <FoodCard
+                                    foodIcon={emoji}
+                                    handler={this.emojiHandler}
+                                />
+                            );
+                        })}
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
