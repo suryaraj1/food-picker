0558831e15b3505a1bc9309dd5f8b1005c4f3421
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-09-07 16:11:54 +0530

    Add click handler to FoodCard

--- src/components/FoodCard/FoodCard.js
+++ src/components/FoodCard/FoodCard.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import './FoodCard.css';
 
-const FlagCard = ({ foodIcon }) => {
-    return (
-        <div className='food-card-wrapper'>
-            <p className='food'>{foodIcon}</p>
-        </div>
-    )
+class FoodCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedEmoji: ""
+        }
+    }
+
+    onClickHandler = () => {
+        const { foodIcon } = this.props;
+        this.setState({
+            selectedEmoji: foodIcon
+        }, () => {
+            const { handler } = this.props;
+            const { selectedEmoji } = this.state;
+            handler(selectedEmoji);
+        })
+    }
+
+
+    render() {
+        const { foodIcon } = this.props;
+        return (
+            <div className='food-card-wrapper' onClick={this.onClickHandler}>
+                <p className='food'>{foodIcon}</p>
+            </div>
+        )
+    }
 }
 
-export default FlagCard;
+export default FoodCard;
