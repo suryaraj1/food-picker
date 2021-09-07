81e93688689c1c701babe94b4789be2070cfae68
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-09-07 21:01:38 +0530

    Add conditional render and UI changes

--- src/components/EmojiMeaning/EmojiMeaning.js
+++ src/components/EmojiMeaning/EmojiMeaning.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import './EmojiMeaning.css';
+import React from "react";
+import "./EmojiMeaning.css";
+
+const isEmojiAbsent = (emojiMeaning) => {
+    return (emojiMeaning === "We don't know this item!");
+}
 
 const EmojiMeaning = ({ emoji, emojiMeaning }) => {
     return (
-        <div className='emoji-meaning-wrapper'>
-            <p className='emoji'>{emoji}</p>
-            <p className='emoji-meaning'>{emojiMeaning}</p>
+        <div className={`emoji-meaning-wrapper ${isEmojiAbsent(emojiMeaning) && "failure"}`}>
+            {isEmojiAbsent(emojiMeaning) ? (
+                <p className="emoji-meaning failure">{emojiMeaning}</p>
+            ) : (
+                <>
+                    <p className="emoji">{emoji}</p>
+                    <p className="emoji-meaning">{emojiMeaning}</p>
+                </>
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default EmojiMeaning;
